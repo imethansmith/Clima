8fe8ee0fa32c98a0e08d9c98ffb453cbcc67dc51
Ethan Smith <edhsmith@icloud.com>
2021-02-24 10:03:30 +1300

    App can now receive JSON for any given city.

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -12,10 +12,38 @@ var cityName = "Auckland"
 let jsonReceiver = JSONDecoder()
 
 struct WeatherManager {
-    let weatherURL = "api.openweathermap.org/data/2.5/weather?&units=metric&appid=254f77517e04920912bf344cb3d6482e"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=254f77517e04920912bf344cb3d6482e"
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        performRequest(urlString: urlString)
     }
+    
+    func performRequest(urlString: String) {
+        // Create URL
+        if let url = URL.init(string: urlString) {
+            // Create a URLSession
+            
+            let session = URLSession(configuration: .default)
+            
+            // Give session a task
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:) )
+            
+            // Start the session task
+            task.resume()
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
+    }
+    
 }
